b06b785411501936b6e13e6264db29017573cd52
KarinMatsuyama <m.karin2@gmail.com>
2020-11-29 19:59:54 -0600

    add junit test for controller

--- src/main/java/com/example/springpractice/model/Customer.java
+++ src/main/java/com/example/springpractice/model/Customer.java
@@ -10,6 +10,12 @@ public class Customer {
     private String name;
     private String email;
 
+    public Customer(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
     public void setId(int id) {
         this.id = id;
     }
